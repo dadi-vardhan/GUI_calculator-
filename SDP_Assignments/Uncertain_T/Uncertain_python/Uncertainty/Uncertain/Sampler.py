723eda5b7765c5eab166866fc05b188955d52541
dadi-vardhan <vishnu.dadi@smail.h-brs.de>
2021-01-10 18:41:29 +0100

    Uncertain package initial commit

---
+++ SDP_Assignments/Uncertain_T/Uncertain_python/Uncertainty/Uncertain/Sampler.py
@@ -0,0 +1,12 @@
+from Uncertain import MarkovChainMonteCarloSampler
+
+class Sampler:
+    def __init__(self):
+        self.model = None
+    def create(self,source):
+        model = MarkovChainMonteCarloSampler(source).MarkovChainMonteCarloSampler()
+        self.model = model
+        return model
+
+    def Take(self,samples):
+        return np.random.choice(self.model,samples)
