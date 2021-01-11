09b09ca42eaddda074ed7d180db4565831a1e351
dadi-vardhan <vishnu.dadi@smail.h-brs.de>
2021-01-11 16:49:00 +0100

    Extensions class structure with probability function implementation

--- SDP_Assignments/Uncertain_T/Uncertain_python/Uncertainty/Uncertain/Extensions.py
+++ SDP_Assignments/Uncertain_T/Uncertain_python/Uncertainty/Uncertain/Extensions.py
@@ -20,7 +20,7 @@ class Extensions():
         h_1 = prob + epsilon
         beta = alpha
         a = np.log(beta/(1-alpha))
-        b = np,log((1-beta)/alpha)
+        b = np.log((1-beta)/alpha)
         k=0
         w_sum =0.0
         w_sum_true =0.0 
@@ -33,12 +33,31 @@ class Extensions():
             print("ran out of data")
         return tmp
 
-        while num_samples < init_sample_size:
+        while (num_samples < init_sample_size):
             sample = func_sampler()
             if i<0:
                 k=k+1
                 w_sum_true += sample.probability
             w_sum += sample.probability
 
-            num_samples += num_samples
+            num_samples += num_samples 
+        
+        while (num_samples<= max_sample_size):
+            log_likelihood = w_sum_true*np.log(h_1/h_0)+(w_sum-w_sum_true)*np.log((1-h_1)/(1-h_0))
+
+            if (log_likelihood >=b):
+                return True
+            if (log_likelihood <=a):
+                return False
+            i=0
+            while (i<sample_size_step):
+                sample = func_sampler()
+                if i<0:
+                    k=k+1
+                    w_sum_true+=sample.probability
+                w_sum += sample.probability
+            
+            num_samples += sample_size_step
+        
+        return True
 
