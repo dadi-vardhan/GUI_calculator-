3e6e195e8bc8710b436afa1692e6dc9e69428b98
HammamWahab <hammam9497@gmail.com>
2021-01-17 11:12:56 +0100

    added MCMC for c++

--- SDP_Assignments/Uncertain_T/cpp/MarkovChainMonteCarloSampler.h
+++ SDP_Assignments/Uncertain_T/cpp/MarkovChainMonteCarloSampler.h
@@ -16,11 +16,11 @@ using namespace std;
 class MarkovChainMonteCarloSampler: public Uncertain<float>{
     public: 
         int size; 
-        // float* distribution; 
         float* new_distribution; 
         int iterations = 5000; 
         float mean(float* x, float s);
         MarkovChainMonteCarloSampler();
+        ~MarkovChainMonteCarloSampler();
         MarkovChainMonteCarloSampler(float* distrib, int s);
         float prior(float* x); 
         float random_normal(float mu, float sigma); 
@@ -31,16 +31,13 @@ class MarkovChainMonteCarloSampler: public Uncertain<float>{
         float* sampling();
 };
 
-MarkovChainMonteCarloSampler::MarkovChainMonteCarloSampler(){
-
-    // this->distribution = new float[8];
-
+MarkovChainMonteCarloSampler::MarkovChainMonteCarloSampler(){}
+MarkovChainMonteCarloSampler::~MarkovChainMonteCarloSampler(){
 }
 
 
 MarkovChainMonteCarloSampler::MarkovChainMonteCarloSampler(float* distrib, int s){
 
-    // this->size = (sizeof(distrib)/sizeof(distrib[0]));
     this->size = s; 
     this->distribution = new float[size];
     for(int i=0; i<size; i++){
@@ -66,40 +63,25 @@ float MarkovChainMonteCarloSampler::random_normal(float mu, float sigma){
 
     // thread_local double z1;
     double z0,z1,mag;
-    // thread_local bool generate;
-    // bool generate;
-    // generate = !generate;
-    // srand(time(0));
-    // int max = 1;
-    // if (!generate)
-    //     return z1;
 
     double u1,u2;
 
 
     do
     {
-        // u1 = rand() * (1.0 / RAND_MAX);
-        // u1 = ((double) rand() / (RAND_MAX)) ;
-        // u1 = dist(mt); 
+
         u1 = this->random_uniform(0,1); 
         u2 = this->random_uniform(0,1); 
 
-
-        // cout << u1 << "u111" << endl; 
-        // u2 = dist(mt); 
-        // u2 = ((double) rand() / (RAND_MAX)) ;
-        // cout << "u1" << u1 << endl; 
     }
     while (u1 <= epsilon);
 
     mag = sigma * sqrt(-2.0*log(u1));
     z0 = mag * cos(two_pi * u2) + mu;
     z1 = mag * sin(two_pi * u2) + mu;
-    // nextGaussianSample = z1; 
+
     float sample = (float) z1; 
-    // cout << "sample" << sample << endl; 
-    // cout << "sample " << sample << endl; 
+
     return sample;
 
 }
@@ -126,7 +108,7 @@ float MarkovChainMonteCarloSampler::likelihood(float* x, float* distrib){
     float b = 0.0;
     float c = 0.0; 
     //dont forget to change 3 into NUM_OF_SAMPLES
-    for(int i=0; i < 3; i++){
+    for(int i=0; i < NUM_OF_SAMPLES; i++){
     a =  -log(x[1] * sqrt(2* M_PI) );
     b = (distrib[i] - x[0])*(distrib[i] - x[0]);
     c = 2*x[1]*x[1]; 
@@ -184,7 +166,6 @@ float* accepted_mean;
 accepted_mean = new float[3000]; 
 float* accepted_sigma; 
 accepted_sigma = new float[3000]; 
-// float rejected[][1] = {}; 
 float* x_new; 
 x_new = new float[2]; 
 float x_lik; 
@@ -198,25 +179,19 @@ float sigmas_sum = 0.0;
 for(int i=0; i<iterations; i++){
     
     x_new = this->transition_model(x); 
-    // for(int i=0; i<2; i++){
-    //     cout << "x_new 0 " << x_new[0] << " x_new 1 " << x_new[1] << endl; 
-    // }
     x_lik = this->likelihood(x,this->distribution); 
     x_new_lik = this->likelihood(x_new,this->distribution); 
     oldtrace = x_lik + log(this->prior(x)); 
     newtrace = x_new_lik + log(this->prior(x_new)); 
     if(this->accept(oldtrace,newtrace)){
-        // cout << "accepted" << endl; 
         for(int i=0; i<2; i++){
             *(x+i) = *(x_new +i); }
-            // cout << *(x) << "," << *(x+1) << endl;
+
 
         accepted_mean[count_accepted] = x_new[0];
         accepted_sigma[count_accepted] = x_new[1];
         means_sum += x_new[0]; 
         sigmas_sum += x_new[1];
-
-        // cout << accepted_mean[count_accepted] << "," << accepted_sigma[count_accepted]<< endl;
 
         count_accepted += 1;
 
@@ -225,22 +200,17 @@ for(int i=0; i<iterations; i++){
 
     }
 
-// cout << "size of accepted mean = " << (sizeof(accepted_mean)/sizeof(*accepted_mean)) << endl; 
-
 
 int burnin = (int)(0.75*count_accepted);
-// cout << "burnin" << burnin << endl; 
+
 int start = count_accepted - burnin;
-// for (int i=start; i<count_accepted; i++){
-//     means_sum += accepted_mean[i];
-//     sigmas_sum += accepted_sigma[i];
-// }
 
 float mu = means_sum/count_accepted;
 float sigma = sigmas_sum/count_accepted;
 
-new_distribution = new float[1000]; 
-for(int i=0; i<1000; i++){
+new_distribution = new float[NUM_OF_SAMPLES]; 
+
+for(int i=0; i<NUM_OF_SAMPLES; i++){
     new_distribution[i] = this->random_normal(mu,sigma);
 }
 
@@ -254,12 +224,6 @@ return model;
 
 
 
-
-
-
-
-
-// return this-> distribution; 
 }
 
 
