c0e2369d8b8a6701125eac26c4728d71c75ef2dc
manummk95 <manummk95@gmail.com>
2021-02-18 23:14:54 +0100

    Converted to PEP8 Style

--- SDP_Assignments/Uncertain_T/Uncertain_python/Uncertainty/UncertainTests/GaussianTests.py
+++ SDP_Assignments/Uncertain_T/Uncertain_python/Uncertainty/UncertainTests/GaussianTests.py
@@ -12,9 +12,9 @@ from Uncertain.Uncertain import Operator
 
 class GaussianTests():
     def gaussian_sample(self):
-        x = Gaussian(5.0,2.0)
-        x=x.get_support()
-        sampler=Sampler()
+        x = Gaussian(5.0, 2.0)
+        x = x.get_support()
+        sampler = Sampler()
         sampler.create(x)
         for i in sampler.take(100):
             assert(i >= -3 and i <= 13)
@@ -22,55 +22,55 @@ class GaussianTests():
     def gaussian_mean(self):
         sum_value = 0.0
         x = Gaussian(5.0,1.0)
-        x=x.get_support()
-        sampler=Sampler()
+        x = x.get_support()
+        sampler = Sampler()
         sampler.create(x)
         for i in sampler.take(100):
             sum_value += i
-        sum_value /=100
-        assert(sum_value>=4.6 and sum_value <=5.4)
+        sum_value /= 100
+        assert(sum_value >= 4.6 and sum_value <= 5.4)
 
     def gaussian_bnn_sample(self):
-        x = Gaussian(1.0,1.0)
-        y = Gaussian(4.0,1.0)
-        z = Operator(x)+Operator(y)
-        sampler=Sampler()
+        x = Gaussian(1.0, 1.0)
+        y = Gaussian(4.0, 1.0)
+        z = Operator(x) + Operator(y)
+        sampler = Sampler()
         sampler.create(z)
         for i in sampler.take(100):
-            assert(i >=-3.0 and i<=13.0) 
+            assert(i >=-3.0 and i <= 13.0) 
 
     def gaussian_bnn_mean(self):
-        x = Gaussian(1.0,1.0)
-        y = Gaussian(4.0,1.0)
-        z = Operator(x)+Operator(y)
-        sum =0.0
-        sampler=Sampler()
+        x = Gaussian(1.0, 1.0)
+        y = Gaussian(4.0, 1.0)
+        z = Operator(x) + Operator(y)
+        sum = 0.0
+        sampler = Sampler()
         sampler.create(z)
         for i in sampler.take(100):
-            sum=sum+i
-        sum = sum/100.0
-        assert(sum>=4.11 and sum <=5.89)
+            sum = sum + i
+        sum = sum / 100.0
+        assert(sum >= 4.11 and sum <= 5.89)
 
     def gaussian_bernoulli_mean(self):
-        x = Gaussian(1.0,1.0)
-        y = Gaussian(3.0,2.0)
-        z = Operator(x)>Operator(y)
-        sampler=Sampler()
+        x = Gaussian(1.0, 1.0)
+        y = Gaussian(3.0, 2.0)
+        z = Operator(x) > Operator(y)
+        sampler = Sampler()
         sampler.create(z)
-        k=0
+        k = 0
         for i in sampler.take(100):
-            if i<0:
-                k=k+1
-        assert (k>=13 and k<52)
+            if i < 0:
+                k = k + 1
+        assert (k >= 13 and k < 52)
     
     def gaussian_bernoulli_conditional(self):
-        x = Gaussian(1.0,1.0)
-        y = Gaussian(4.0,2.0)
-        extns=Extensions()
-        z1 = Operator(x)>Operator(y)
-        z2= Operator(y)<Operator(x)
-        z3 = Operator(y)>Operator(x)
-        z4 = Operator(x)<Operator(y)
+        x = Gaussian(1.0, 1.0)
+        y = Gaussian(4.0, 2.0)
+        extns = Extensions()
+        z1 = Operator(x) > Operator(y)
+        z2 = Operator(y) < Operator(x)
+        z3 = Operator(y) > Operator(x)
+        z4 = Operator(x) < Operator(y)
         if extns.pr(z1):
             print("X > Y evaluates true, incorrectly")
         if extns.pr(z2):
